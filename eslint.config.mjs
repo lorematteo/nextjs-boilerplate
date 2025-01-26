9cbb29c1d719b6d9680bc63b0698c1190bf5cfea
lorematteo <loremattteo@gmail.com>
2025-01-26 20:16:06 +0100

    build(deps): adding various eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,91 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    ignores: [
+      '**/node_modules/*',
+      '**/out/*',
+      '**/.next/*',
+      '**/coverage',
+      'src/styles/globals.css',
+      '*.mjs',
+    ],
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:jsx-a11y/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ),
+  {
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 11,
+      sourceType: 'module',
 
-export default eslintConfig;
+      parserOptions: {
+        project: true,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'no-console': [
+        2,
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+    },
+  },
+];
