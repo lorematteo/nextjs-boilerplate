ece36042c665a5469fbf27179def0d4c0514235a
lorematteo <loremattteo@gmail.com>
2025-01-26 18:53:11 +0100

    chore(lint): editing commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^([a-z]+)?\s*\((.+?)\): (.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'build',
+        'docs',
+        'feat',
+        'evol',
+        'fix',
+        'style',
+        'refactor',
+        'test',
+        'ci',
+        'revert',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+  },
+};
